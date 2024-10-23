9cfb63ae1cbcdac7f8a873411c789472359fd3fb
Mariii <139721644+mariiiporfirio@users.noreply.github.com>
2024-10-23 10:41:11 -0300

    Update app.js

--- senac/nodejs/app.js
+++ senac/nodejs/app.js
@@ -4,17 +4,31 @@ const express = require ('express');
 const app = express();
 app.set('view engine', 'ejs');
 
-app.listen(3001);
-
 // Acessando a rota através do get
 app.get('/', (req, res) => {
     // res.sendFile('./views/index.html', {root: __dirname});
-    res.render('index');
+
+    // passando parâmetro body
+    const blogs = [
+        {titulo: 'Rosa', conteudo: 'Rosas são vermelhas.'},
+        {titulo: 'Girassol', conteudo: 'Sempre em direção ao Sol.'},
+        {titulo: 'Margaridas', conteudo: 'Um tipo de flor.'},
+    ];
+
+    // Acessando a rota através do Framework ejs após instalação
+    res.render('index', {titulo: 'Home', blogs}); // passando parâmetro head
 });
 
 app.get('/sobre', (req, res) => {
     // res.sendFile('./views/sobre.html', {root: __dirname});
-    res.render('sobre');
+    
+    const blogs = [
+        {titulo: 'Rosa', conteudo: 'Rosas são vermelhas.'},
+        {titulo: 'Girassol', conteudo: 'Sempre em direção ao Sol.'},
+        {titulo: 'Margaridas', conteudo: 'Um tipo de flor.'},
+    ];
+    
+    res.render('sobre', {titulo: 'Sobre', blogs});
 });
 
 // Redirecionamento de página
@@ -22,9 +36,13 @@ app.get('/sobrenos', (req, res) => {
     res.redirect('sobre');
 })
 
+app.get('/blog/criar', (req, res) => {
+    res.render('criar', {titulo: 'Criar Novo Blog'});
+})
 
 app.use((req, res) => {
     // res.status(400).sendFile('./views/404.html', {root: __dirname});
-    // Acessando a rota através do Framework ejs após instalação
     res.status(404).render('404');
 });
+
+app.listen(3001);
